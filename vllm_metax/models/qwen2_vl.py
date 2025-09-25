09ed7d3379a64072dee801985b7f2f30ca3e94b6
tomorin123 <724870636@qq.com>
2025-09-25 14:40:36 +0800

    Update qwen2_vl.py

--- vllm_metax/models/qwen2_vl.py
+++ vllm_metax/models/qwen2_vl.py
@@ -1016,11 +1016,12 @@ class Qwen2VLMultiModalProcessor(BaseMultiModalProcessor[Qwen2VLProcessingInfo]
         prompt: str,
         mm_data: Mapping[str, object],
         mm_kwargs: Mapping[str, object],
+        tok_kwargs: Mapping[str, object],
     ) -> BatchFeature:
         return self.info.ctx.call_hf_processor(
             self.info.get_hf_processor(**mm_kwargs),
             dict(text=prompt, **mm_data),
-            self.info._get_image_processor_kwargs(**mm_kwargs),
+            dict(**mm_kwargs, **tok_kwargs),
         )
 
     def _get_prompt_updates(
