f2edc9bf7d401f2de4143252de2c4d2ff79ffefe
yi.zhang2 <yi.zhang2@metax-tech.com>
2025-11-20 12:14:30 +0800

    [chore] Add metax modification copyright labels
    Signed-off-by: yi.zhang2 <yi.zhang2@metax-tech.com>

--- csrc/mamba/mamba_ssm/selective_scan_fwd.cu
+++ csrc/mamba/mamba_ssm/selective_scan_fwd.cu
@@ -1,3 +1,5 @@
+// 2025 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights
+// Reserved.
 // clang-format off
 // adapted from https://github.com/state-spaces/mamba/blob/main/csrc/selective_scan/selective_scan_fwd_kernel.cuh
 #include <torch/all.h>
--- vllm_metax/models/baichuan.py
+++ vllm_metax/models/baichuan.py
@@ -1,3 +1,4 @@
+# 2025 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # Copyright 2022 EleutherAI and the HuggingFace Inc. team. All rights reserved.
