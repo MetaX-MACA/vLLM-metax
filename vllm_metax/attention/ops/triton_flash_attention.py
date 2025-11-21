800ca7c6527f08247113a638a44844993e7c2a2d
metax-yi1zhang <yi.zhang2@metax-tech.com>
2025-11-21 17:12:56 +0800

    Copyrights: update metax modification copyrights labels (#153)
    Signed-off-by: yi.zhang2 <yi.zhang2@metax-tech.com>

--- vllm_metax/attention/ops/triton_flash_attention.py
+++ vllm_metax/attention/ops/triton_flash_attention.py
@@ -1,3 +1,4 @@
+# 2025 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
 #!/usr/bin/env python
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
--- vllm_metax/patch/custom_op/__init__.py
+++ vllm_metax/patch/custom_op/__init__.py
@@ -1,2 +1,3 @@
+# 2025 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 from . import custom_op
