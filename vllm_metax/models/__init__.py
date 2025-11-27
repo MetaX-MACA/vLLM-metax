8116d87420160f7c7b759e6b58550e4c3b0e8267
Hank <hcc.mayday@gmail.com>
2025-11-27 15:45:48 +0800

    remove baichuan model registry in plugin
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/models/__init__.py
+++ vllm_metax/models/__init__.py
@@ -4,14 +4,14 @@ from vllm import ModelRegistry
 
 
 def register_model():
-    ModelRegistry.register_model(
-        "BaichuanForCausalLM", "vllm_metax.models.baichuan:BaichuanForCausalLM"
-    )
+    # ModelRegistry.register_model(
+    #     "BaichuanForCausalLM", "vllm_metax.models.baichuan:BaichuanForCausalLM"
+    # )
 
-    ModelRegistry.register_model(
-        "BaiChuanMoEForCausalLM",
-        "vllm_metax.models.baichuan_moe:BaiChuanMoEForCausalLM",
-    )
+    # ModelRegistry.register_model(
+    #     "BaiChuanMoEForCausalLM",
+    #     "vllm_metax.models.baichuan_moe:BaiChuanMoEForCausalLM",
+    # )
 
     ModelRegistry.register_model(
         "Qwen2VLForConditionalGeneration",
