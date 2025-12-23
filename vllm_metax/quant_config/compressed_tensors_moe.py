f77fe6ba54c65d9991fa0485f3e1d1465012e35f
Hank <hcc.mayday@gmail.com>
2025-12-23 14:16:33 +0800

    refactor w8a8 and compressed tensor moe config
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/quant_config/compressed_tensors_moe.py
+++ vllm_metax/quant_config/compressed_tensors_moe.py
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 import torch
-from typing import Callable
 from vllm.model_executor.layers.fused_moe import FusedMoE
 from vllm.model_executor.layers.quantization.compressed_tensors import (
     compressed_tensors_moe as vllm_ctm,
@@ -18,44 +17,50 @@ class CompressedTensorsMoEMethod(vllm_ctm.CompressedTensorsMoEMethod):
         layer: torch.nn.Module,
         layer_name: str,
     ) -> "CompressedTensorsMoEMethod":
-        moe_method = vllm_ctm.CompressedTensorsMoEMethod.get_moe_method(
+        origin_moe_method = vllm_ctm.CompressedTensorsMoEMethod.get_moe_method(
             quant_config, layer, layer_name
         )
-        # Replace with Metax's MoE quantization methods
-        if isinstance(moe_method, vllm_ctm.CompressedTensorsWNA16MoEMethod):
-            moe_method = CompressedTensorsWNA16MoEMethod(
-                quant_config, layer.moe_config, layer_name
+
+        # -------------------------------------------
+        # Note: all these are copied from vllm's logic
+        #       we just need the
+        #           - `weight_quant`
+        #           - `input_quant`
+        #       to construct the corresponding class.
+        quant_config._add_fused_moe_to_target_scheme_map()
+        unfused_names = [
+            layer_name + proj_name
+            for proj_name in [".0.gate_proj", ".0.up_proj", ".0.down_proj"]
+        ]
+        all_scheme_dicts = [
+            quant_config.get_scheme_dict(layer, name) for name in unfused_names
+        ]
+        scheme_dict = all_scheme_dicts.pop()
+        if not all([cur_dict == scheme_dict for cur_dict in all_scheme_dicts]):
+            raise ValueError(
+                "All MoE projections need to have same "
+                "quantization scheme but found multiple"
             )
-        elif isinstance(moe_method, vllm_ctm.CompressedTensorsW8A8Int8MoEMethod):
-            moe_method = CompressedTensorsW8A8Int8MoEMethod(
-                quant_config, layer.moe_config, layer_name
+        weight_quant = scheme_dict.get("weights")
+        input_quant = scheme_dict.get("input_activations")
+
+        # -------------------------------------------
+        # Replace with Metax's MoE quantization methods by:
+        #  - `weights_quant`
+        #  - `input_quant`
+        if isinstance(origin_moe_method, vllm_ctm.CompressedTensorsWNA16MoEMethod):
+            return CompressedTensorsWNA16MoEMethod(
+                weight_quant, input_quant, layer.moe_config
             )
-        return moe_method
+        elif isinstance(origin_moe_method, vllm_ctm.CompressedTensorsW8A8Int8MoEMethod):
+            return CompressedTensorsW8A8Int8MoEMethod(
+                weight_quant, input_quant, layer.moe_config
+            )
 
 
 class CompressedTensorsW8A8Int8MoEMethod(vllm_ctm.CompressedTensorsW8A8Int8MoEMethod):
     def apply(
-        self,
-        layer: FusedMoE,
-        x: torch.Tensor,
-        router_logits: torch.Tensor,
-        top_k: int,
-        renormalize: bool,
-        use_grouped_topk: bool = False,
-        topk_group: int | None = None,
-        num_expert_group: int | None = None,
-        global_num_experts: int = -1,
-        expert_map: torch.Tensor | None = None,
-        custom_routing_function: Callable | None = None,
-        scoring_func: str = "softmax",
-        routed_scaling_factor: float = 1.0,
-        e_score_correction_bias: torch.Tensor | None = None,
-        apply_router_weight_on_input: bool = False,
-        activation: str = "silu",
-        enable_eplb: bool = False,
-        expert_load_view: torch.Tensor | None = None,
-        logical_to_physical_map: torch.Tensor | None = None,
-        logical_replica_count: torch.Tensor | None = None,
+        self, layer: FusedMoE, x: torch.Tensor, router_logits: torch.Tensor
     ) -> torch.Tensor | tuple[torch.Tensor, torch.Tensor]:
         from vllm_metax.model_executor.layers.fused_moe import fused_experts
 
@@ -71,10 +76,10 @@ class CompressedTensorsW8A8Int8MoEMethod(vllm_ctm.CompressedTensorsW8A8Int8MoEMe
             topk_weights=topk_weights,
             topk_ids=topk_ids,
             inplace=True,
-            activation=activation,
-            apply_router_weight_on_input=apply_router_weight_on_input,
-            global_num_experts=global_num_experts,
-            expert_map=expert_map,
+            activation=layer.activation,
+            apply_router_weight_on_input=layer.apply_router_weight_on_input,
+            global_num_experts=layer.global_num_experts,
+            expert_map=layer.expert_map,
             quant_config=self.moe_quant_config,
         )
 
@@ -85,23 +90,6 @@ class CompressedTensorsWNA16MoEMethod(vllm_ctm.CompressedTensorsWNA16MoEMethod):
         layer: FusedMoE,
         x: torch.Tensor,
         router_logits: torch.Tensor,
-        top_k: int,
-        renormalize: bool,
-        use_grouped_topk: bool = False,
-        topk_group: int | None = None,
-        num_expert_group: int | None = None,
-        global_num_experts: int = -1,
-        expert_map: torch.Tensor | None = None,
-        custom_routing_function: Callable | None = None,
-        scoring_func: str = "softmax",
-        routed_scaling_factor: float = 1.0,
-        e_score_correction_bias: torch.Tensor | None = None,
-        apply_router_weight_on_input: bool = False,
-        activation: str = "silu",
-        enable_eplb: bool = False,
-        expert_load_view: torch.Tensor | None = None,
-        logical_to_physical_map: torch.Tensor | None = None,
-        logical_replica_count: torch.Tensor | None = None,
     ) -> torch.Tensor | tuple[torch.Tensor, torch.Tensor]:
         from vllm_metax.model_executor.layers.fused_moe import fused_experts
 
@@ -117,9 +105,9 @@ class CompressedTensorsWNA16MoEMethod(vllm_ctm.CompressedTensorsWNA16MoEMethod):
             topk_weights=topk_weights,
             topk_ids=topk_ids,
             inplace=True,
-            activation=activation,
-            apply_router_weight_on_input=apply_router_weight_on_input,
-            global_num_experts=global_num_experts,
-            expert_map=expert_map,
+            activation=layer.activation,
+            apply_router_weight_on_input=layer.apply_router_weight_on_input,
+            global_num_experts=layer.global_num_experts,
+            expert_map=layer.expert_map,
             quant_config=self.moe_quant_config,
         )
