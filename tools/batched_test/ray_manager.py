2ba00a2bd4aacbc25774d1d4b5ce446a91a80324
Hank <hcc.mayday@gmail.com>
2026-01-12 19:12:40 +0800

    bug fix
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- tools/batched_test/ray_manager.py
+++ tools/batched_test/ray_manager.py
@@ -166,7 +166,7 @@ class RayClusterManager:
         extra_env = {
             "GLOO_SOCKET_IFNAME": master.nic,
             "NCCL_SOCKET_IFNAME": master.nic,
-            "MACA_PATH": "opt/maca",
+            "MACA_PATH": "/opt/maca",
             "RAY_EXPERIMENTAL_NOSET_CUDA_VISIBLE_DEVICES": "1",
         }
         extra_env.update(extra_serve_env)
@@ -202,7 +202,7 @@ class RayClusterManager:
             extra_env = {
                 "GLOO_SOCKET_IFNAME": slave.nic,
                 "NCCL_SOCKET_IFNAME": slave.nic,
-                "MACA_PATH": "opt/maca",
+                "MACA_PATH": "/opt/maca",
                 "RAY_EXPERIMENTAL_NOSET_CUDA_VISIBLE_DEVICES": "1",
             }
             extra_env.update(extra_serve_env)
