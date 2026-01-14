c3fe2ba01898e3c16bb41867fe81b6f0648ae2ca
Hank <hcc.mayday@gmail.com>
2026-01-14 19:07:01 +0800

    update requirements and docker file
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- use_existing_metax.py
+++ use_existing_metax.py
@@ -1,5 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
-# 2026 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 import glob
@@ -10,11 +9,19 @@ for file in requires_files:
     print(f">>> cleaning {file}")
     with open(file) as f:
         lines = f.readlines()
-    if "+metax" in "".join(lines).lower():
+    if (
+        "+metax" in "".join(lines).lower()
+        or "+maca" in "".join(lines).lower()
+        or "mcpy" in "".join(lines).lower()
+    ):
         print("removed:")
         with open(file, "w") as f:
             for line in lines:
-                if "+metax" not in line.lower():
+                if (
+                    "+metax" not in line.lower()
+                    and "+maca" not in line.lower()
+                    and "mcpy" not in line.lower()
+                ):
                     f.write(line)
                 else:
                     print(line.strip())
