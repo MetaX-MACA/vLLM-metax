361f8224ece5d49acc4317258b35edef6d573f49
Hank <hcc.mayday@gmail.com>
2026-02-26 16:05:08 +0800

    update patches for lora
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/patch/__init__.py
+++ vllm_metax/patch/__init__.py
@@ -1,5 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
-from . import maca_visible_device  # noqa: F401
 from . import distributed  # noqa: F401
 from . import device_allocator  # noqa: F401
 from . import model_executor  # noqa: F401
@@ -7,3 +6,6 @@ from . import model_executor  # noqa: F401
 from . import dp_fix  # noqa: F401
 from . import quant_kernels  # noqa: F401
 from . import triton_support  # noqa: F401
+
+from . import chores  # noqa: F401
+from . import lora  # noqa: F401
