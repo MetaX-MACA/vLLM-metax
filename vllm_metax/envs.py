e96d74fbc27a0d3a4517ce85f4f32a419423066c
Hank <hcc.mayday@gmail.com>
2026-03-03 15:00:51 +0800

    update: add env control for split-forward in fa2 while dcp==1
    Note!: VLLM_METAX_ENABLE_FA_SPLIT_FORWARD must be disable if using
    slide window attention on fa2.
    
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/envs.py
+++ vllm_metax/envs.py
@@ -12,6 +12,9 @@ if TYPE_CHECKING:
     CMAKE_BUILD_TYPE: str | None
     VERBOSE: bool = False
     MACA_DP_OPT: bool = False
+    MACA_VLLM_ENABLE_MCTLASS_PYTHON_API: bool = False
+    MACA_VLLM_ENABLE_MCTLASS_FUSED_MOE: bool = False
+    VLLM_METAX_ENABLE_FA_SPLIT_FORWARD: bool = True
 
 environment_variables: dict[str, Callable[[], Any]] = {
     # ================== Installation Time Env Vars ==================
@@ -57,6 +60,12 @@ environment_variables: dict[str, Callable[[], Any]] = {
     ),
     # if set, enable combine allreduce all2all
     "MACA_DP_OPT": lambda: bool(int(os.environ.get("MACA_DP_OPT", "0"))),
+    # if set, enable FA split forward into
+    # prefill and decode for better latency
+    # and memory usage during decoding
+    "VLLM_METAX_ENABLE_FA_SPLIT_FORWARD": lambda: bool(
+        int(os.environ.get("VLLM_METAX_ENABLE_FA_SPLIT_FORWARD", "1"))
+    ),
 }
 
 
