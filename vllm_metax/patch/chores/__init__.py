ede824116c0194f222527a005da7ff89bc511dbe
Lianjie Zhang <lianjie.zhang@metax-tech.com>
2026-03-18 23:17:52 +0800

    set default value of async_scheduling to False
    Change-Id: Id428894f2cab811aa16b239a62c2967fdeaf51bc
    Signed-off-by: Lianjie Zhang <lianjie.zhang@metax-tech.com>

--- vllm_metax/patch/chores/__init__.py
+++ vllm_metax/patch/chores/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # 2026 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
+from . import vllm_serve_args  # noqa: F401
 from . import maca_visible_device  # noqa: F401
 from . import bench_serve_args  # noqa: F401
 from . import remove_error_log  # noqa: F401
