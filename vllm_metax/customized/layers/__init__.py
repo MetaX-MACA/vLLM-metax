77737b6943a8dfd8adaabf8e06036a9b12c68f68
Hank <hcc.mayday@gmail.com>
2026-05-09 14:11:37 +0800

    [BugFix] Fix k_norm weight sharding in MiniMaxM2Attention when total_num_kv_heads < tp_size (#38191)
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/customized/layers/__init__.py
+++ vllm_metax/customized/layers/__init__.py
@@ -1,3 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # 2026 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
-from . import mm_encoder_attention, unquantized_fused_moe_method, sparse_attn_indexer  # noqa: F401
+from . import mm_encoder_attention  # noqa: F401
+from . import unquantized_fused_moe_method  # noqa: F401
+from . import sparse_attn_indexer  # noqa: F401
+from . import minimax_text01_rmsnorm  # noqa: F401
