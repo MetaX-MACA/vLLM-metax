7d1bbd551f7a1613f3ff4a9f207aab3a32007943
mengz <2567587994@qq.com>
2026-06-22 12:52:24 +0800

    fix: harden env.sh for nounset shells

--- env.sh
+++ env.sh
@@ -4,11 +4,11 @@ export MACA_PATH=${1:-$DEFAULT_DIR}
 
 # cu-bridge
 export CUCC_PATH="${CUCC_PATH:-${MACA_PATH}/tools/cu-bridge}"
-if [ -z "${CUDA_PATH}" ]; then
+if [ -z "${CUDA_PATH:-}" ]; then
   if [ -d "${CUCC_PATH}/CUDA_DIR" ]; then
     export CUDA_PATH="${CUCC_PATH}/CUDA_DIR"
-  elif [ -d "${HOME}/cu-bridge/CUDA_DIR" ]; then
-    export CUDA_PATH="${HOME}/cu-bridge/CUDA_DIR"
+  elif [ -d "${HOME:-}/cu-bridge/CUDA_DIR" ]; then
+    export CUDA_PATH="${HOME:-}/cu-bridge/CUDA_DIR"
   else
     export CUDA_PATH="${CUCC_PATH}"
   fi
@@ -17,6 +17,6 @@ export CUCC_CMAKE_ENTRY=2
 
 # update PATH
 export PATH=${MACA_PATH}/mxgpu_llvm/bin:${MACA_PATH}/bin:${CUCC_PATH}/tools:${CUCC_PATH}/bin:${PATH}
-export LD_LIBRARY_PATH=${MACA_PATH}/lib:${MACA_PATH}/ompi/lib:${MACA_PATH}/mxgpu_llvm/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${MACA_PATH}/lib:${MACA_PATH}/ompi/lib:${MACA_PATH}/mxgpu_llvm/lib:${LD_LIBRARY_PATH:-}
 
 export VLLM_INSTALL_PUNICA_KERNELS=1
