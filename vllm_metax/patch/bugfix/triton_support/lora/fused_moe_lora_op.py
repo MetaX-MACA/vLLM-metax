45b1f489770782e6f82f21a2798172f73ba5fde3
Hank <hcc.mayday@gmail.com>
2026-07-02 10:13:49 +0000

    [patch] update patches
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/patch/bugfix/triton_support/lora/fused_moe_lora_op.py
+++ vllm_metax/patch/bugfix/triton_support/lora/fused_moe_lora_op.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # 2026 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
+
+
 # -----------------------------------------------
 # Note: Disable unsupported Triton ops in fused MoE LoRA kernels.
 #
