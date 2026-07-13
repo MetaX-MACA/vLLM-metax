82b8d039afb08751f24aeec03bb39e4aa2b8753e
lli <liangju.li@metax-tech.com>
2026-07-13 18:14:11 +0800

    [PAE-13762]minimax_qk_norm_fusion
    Change-Id: Id38e2b4f5407348e0953464f8f22b6192568e250
    Signed-off-by: lli <liangju.li@metax-tech.com>
    (cherry picked from commit a1f0d7c520eb4f4b8b4c4d5342825e2754abacce)

---
+++ vllm_metax/patch/model_executor/layers/__init__.py
@@ -0,0 +1,5 @@
+
+# SPDX-License-Identifier: Apache-2.0
+# 2026 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
+from . import lamport_workspace
+from . import rms_norm_tp
