9c1f5290d64a77e8f3d7f9d183008ef8e755a0da
Hank <hcc.mayday@gmail.com>
2026-08-03 17:44:30 +0800

    [chores] sync upstream interface
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/registry/custom_ops/layers/fused_moe/unquantized_fused_moe_method.py
+++ vllm_metax/registry/custom_ops/layers/fused_moe/unquantized_fused_moe_method.py
@@ -52,7 +52,7 @@ class UnquantizedFusedMoEMethod(vllm_UnquantizedFusedMoEMethod):
         # Shuffle weights to runtime format.
         w13_new, w2_new = convert_to_unquantized_kernel_format(
             self.unquantized_backend,
-            layer=layer,
+            moe_config=layer.moe_config,
             w13_weight=w13,
             w2_weight=w2,
         )
